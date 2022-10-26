c70ff87bd0c232639f23a9b50e8783f122ea4c24
Christos Marroquin <christos.marroquin@gmail.com>
2022-10-25 23:56:13 -0500

    iexact

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
@@ -8,34 +8,31 @@ def home(request):
     return render(request, "index.html", context)
 
 def detallePost(request, slug):
-    post = Post.objects.get(slug = slug)
-    print("--------------")
-    print(post)
-    print("--------------")
+    post = get_object_or_404(Post, slug = slug)
     context = {"post":post}
     return render(request, "post.html", context)
 
 def generales(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre="General"))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact ="General"))
     context = {"posts":posts}
     return render(request, "generales.html", context)
 
 def programacion(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre="Programacion"))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact="Programacion"))
     context = {"posts":posts}
     return render(request, "programacion.html",context)
 
 def tutoriales(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre="Tutoriales"))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact="Tutoriales"))
     context = {"posts":posts}
     return render(request, "tutoriales.html",context)
 
 def tecnologia(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre="Tecnologia"))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact="Tecnologia"))
     context = {"posts":posts}
     return render(request, "tecnologia.html",context)
 
 def videojuegos(request):
-    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre="Videojuegos"))
+    posts = Post.objects.filter(estado = True, categoria = Categoria.objects.get(nombre__iexact="Videojuegos"))
     context = {"posts":posts}
     return render(request, "videojuegos.html",context)
