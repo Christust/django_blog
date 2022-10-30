55db33345bea0122fa6a24b771764b0bc34ba4ce
Christos Marroquin <christos.marroquin@hotmail.com>
2022-10-30 02:09:26 -0600

    adminPost

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -14,6 +14,10 @@ class AutorResource(resources.ModelResource):
     class Meta:
         model = Autor
 
+class PostResource(resources.ModelResource):
+    class Meta:
+        model = Post
+
 # Agregamos una herencia mas que es ImportExportModelAdmin y el atributo resource_class el cual es la clase rescource creada anteriormente
 class CategoriaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     # Agregar barra de busqueda
@@ -27,7 +31,12 @@ class AutorAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ("nombres", "apellidos", "correo", "estado", "fecha_creacion")
     resource_class = AutorResource
 
+class PostAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ["titulo", "slug", "descripcion"]
+    list_display = ("titulo", "slug", "descripcion", "fecha_creacion")
+    resource_class = PostResource
+
 # Register your models here.
 admin.site.register(Autor, AutorAdmin)
 admin.site.register(Categoria, CategoriaAdmin)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
